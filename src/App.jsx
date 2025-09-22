f13ae279bee7fcf99ee7493844173ed418436d0b
Shubh-Patil311 <shubhampatil@gtasterix>
2025-09-22 11:51:30 +0530

    Updated committee

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import About from "./components/About";
-// import Events from "./components/";
+
 import Contact from "./components/Contact";
 import Training from "./components/Training";
 import Footer from "./components/Footer";
